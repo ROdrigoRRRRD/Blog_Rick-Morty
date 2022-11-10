01d136b52de8d046d02614ef89eff7a1d52570bc
ROdrigoRRRRD <roddrigo2003cod@gmail.com>
2022-11-10 17:53:35 -0600

    Final ahorasi

--- Characters.php
+++ Characters.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="Icon" href="./images/R&M.ico"/>
-    <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="Styles.css">
+    <title>Personajes</title>
 </head>
 <body>
     <nav class="navbar">
